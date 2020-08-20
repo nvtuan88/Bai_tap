3f3e5c7bbfa934e5b2e19593e55faa424c1af4f7
Nguyễn Văn Tuấn <nvtuan88@gmail.com>
2020-08-20 18:38:23 +0700

    Tuan

---
+++ UWP_K3/UWP_K3/Entity/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWP_K3.Entity
+{
+    public class Student
+    {
+        public string RollNumber { get; set; }
+        public string FullName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+        public StudentStatus Status { get; set; }
+
+        public bool IsNewStudent()
+        {
+            return CreatedAt >= DateTime.Now.AddMonths(-2);
+        }
+
+        public bool IsDeactive()
+        {
+            return Status == StudentStatus.DEACTIVE;
+        }
+
+        public override string ToString()
+        {
+            return $"Fullname {FullName}, rollnumber {RollNumber}";
+        }
+    }
+    public enum StudentStatus
+    {
+        ACTIVE = 1,
+        DEACTIVE = 0
+    }
+}
---
+++ UWP_K3/UWP_K3/ViewModels/MainPageStudent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Appointments.AppointmentsProvider;
+using UWP_K3.Models;
+
+namespace UWP_K3.ViewModels
+{
+    public class MainPageStudent
+    {
+        public MainPageStudent() {
+            for (int i = 0; i < 10; i++)
+            {
+                var item = new Models.StudenItem()
+                {
+                    id = string.Format("Task Title {0}", i),
+                    name = string.Format("Student {0}", i),
+                    gpa = string.Format("gpa {0}", i)
+                };
+                this.Items.Add(item);
+            }
+        }
+        public ObservableCollection<Models.StudenItem> Items { get; private set; } = new ObservableCollection<Models.StudenItem>();
+    }
+}
