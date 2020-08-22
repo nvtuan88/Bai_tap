1ffa5ac92fb1845e40af69f496515263baef4ad2
Nguyễn Văn Tuấn <nvtuan88@gmail.com>
2020-08-22 11:52:34 +0700

    Up

--- UWP_K3/UWP_K3/App.xaml.cs
+++ UWP_K3/UWP_K3/App.xaml.cs
@@ -66,7 +66,7 @@ namespace UWP_K3
                     // When the navigation stack isn't restored navigate to the first page,
                     // configuring the new page by passing required information as a navigation
                     // parameter
-                    rootFrame.Navigate(typeof(Views.MainStudent), e.Arguments);
+                    rootFrame.Navigate(typeof(Views.MainGoNut), e.Arguments);
                 }
                 // Ensure the current window is active
                 Window.Current.Activate();
